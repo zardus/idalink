4052f04ba78ec1f3240d300929f5f1e077a87242
Yan <yans@yancomm.net>
2015-05-05 19:23:36 -0700

    add requirements and install the embedded rpyc

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from distutils.core import setup
 setup(
     name = 'idalink',
     version = '0.01',
-    packages = ['idalink'],
-    package_data = { 'idalink': [ 'support/*.sh' ] }
+    packages = ['idalink', 'idalink.rpyc'],
+    package_data = { 'idalink': [ 'support/*.sh' ] },
+    install_requires=[i.strip() for i in open('requirements.txt').readlines() if 'git' not in i]
 )
