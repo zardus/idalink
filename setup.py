c3743d39be2b2ee0d9df5d83ff007130753b8cde
Yan <yans@yancomm.net>
2015-05-05 19:25:33 -0700

    install the rpyc subpackages

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
     name = 'idalink',
     version = '0.01',
-    packages = ['idalink', 'idalink.rpyc'],
+    packages = ['idalink', 'idalink.rpyc', 'idalink.rpyc.core', 'idalink.rpyc.scripts', 'idalink.rpyc.utils', 'idalink.rpyc.lib'],
     package_data = { 'idalink': [ 'support/*.sh' ] },
     install_requires=[i.strip() for i in open('requirements.txt').readlines() if 'git' not in i]
 )
