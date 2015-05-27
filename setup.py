1e5f7664cc0bd532a12118b7e30430989a00cc01
Yan <yans@yancomm.net>
2015-05-27 02:51:26 -0700

    description and version bump

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 from distutils.core import setup
 setup(
     name = 'idalink',
-    version = '0.01',
+    description = 'An interface to the insides of IDA!',
+    version = '0.10',
     packages = ['idalink', 'idalink.rpyc', 'idalink.rpyc.core', 'idalink.rpyc.scripts', 'idalink.rpyc.utils', 'idalink.rpyc.lib'],
     package_data = { 'idalink': [ 'support/*.sh' ] },
     install_requires=['rpyc']
