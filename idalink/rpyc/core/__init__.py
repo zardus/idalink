7cea20535a6c5c8d5406ba3286a62653eaf4ea98
Yan <yans@yancomm.net>
2015-08-11 01:31:08 -0700

    removed rpyc

--- idalink/rpyc/core/__init__.py
+++
@@ -1,7 +0,0 @@
-from .stream import SocketStream, TunneledSocketStream, PipeStream
-from .channel import Channel
-from .protocol import Connection
-from .netref import BaseNetref
-from .async import AsyncResult, AsyncResultTimeout
-from .service import Service, VoidService, SlaveService
-from .vinegar import GenericException
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ setup(
     name = 'idalink',
     description = 'An interface to the insides of IDA!',
     version = '0.10',
-    packages = ['idalink', 'idalink.rpyc', 'idalink.rpyc.core', 'idalink.rpyc.scripts', 'idalink.rpyc.utils', 'idalink.rpyc.lib'],
+    packages = ['idalink'],
     package_data = { 'idalink': [ 'support/*.sh' ] },
     install_requires=['rpyc']
 )
