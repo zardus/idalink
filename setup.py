f31436c26534a2362e232eaeacd4cb40e2a9cef3
Audrey Dutcher <audrey@rhelmot.io>
2018-10-17 14:27:19 -0700

    Tick version number

--- setup.py
+++ setup.py
@@ -4,13 +4,10 @@ setup(
     name='idalink',
     description='An interface to the insides of IDA!',
     long_description=open('README.md').read(),
-    version='0.10',
+    version='0.11',
     url='https://github.com/zardus/idalink',
     license='GNU General Public License v3',
     packages=find_packages(),
-    package_data={
-        'idalink': ['support/*'],
-    },
     install_requires=[
         'rpyc',
     ],
